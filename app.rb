139e56538b17fa969a8817297112b8ccb5093ce6
J <jquarites@gmail.com>
2014-10-03 12:52:07 -0500

    added connection to pg and hability to generate txt

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'cuba'
+require 'pg'
 
 Cuba.define do
   on root do
     res.write("HI")
+    out_file = 
+File.new("out.txt", "w")
+    conn = PG.connect( dbname: 'lingodb', user: 'lingouser', password: 
+'lingopwd')
+    conn.exec("SELECT * FROM pg_stat_activity") do |result|
+      puts "creating txt file"
+      #TODO verify why it creates a new line
+      out_file.write(result.first.values_at('pid','usename').push(" 
+~").join(" "))
+      out_file.close
+    end
   end
 end
