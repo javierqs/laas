227e2e5f668d44cc9dd803c4e0276409366e9563
J <jquarites@gmail.com>
2014-10-03 22:01:16 -0500

    started using parameters

--- app.rb
+++ app.rb
@@ -1,15 +1,36 @@
 require 'cuba'
 require 'pg'
 
+msg = "Received req"
+
+connection_hash = {
+                    dbname: 'lingodb',
+                    user: 'lingouser',
+                    password: 'lingopwd' 
+                  }
+
+conn = PG.connect(connection_hash)
+
+#main_query = "SELECT * FROM pg_stat_activity"
+main_query = "select schemaname, tablename, tableowner from pg_catalog.pg_tables limit 5"
+
+query_result_filename = "out.txt"
+
+c = %w(schemaname tablename tableowner)
+
 Cuba.define do
   on root do
-    res.write("HI")
-    out_file = File.new("out.txt", "w")
-    conn = PG.connect( dbname: 'lingodb', user: 'lingouser', password: 'lingopwd')
-    conn.exec("SELECT * FROM pg_stat_activity") do |result|
+    res.write(msg)
+    out_file = File.new(query_result_filename, "w")
+    
+    conn.exec(main_query) do |result|
       puts "creating txt file"
-      out_file.write(result.first.values_at('pid','usename').push(" ~").join(" "))
-      out_file.close
+      result.each do |row|
+        out_file.write(row.values_at(*c).push(" ~\n").join(" "))
+      end
     end
+    
+    out_file.close
+
   end
 end
